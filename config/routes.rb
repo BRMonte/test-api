43b0c285058492df90b176dcd6daf1ad7885b6ac
Bruno Montebello <brunormontebello@hotmail.com>
2021-04-26 23:00:51 +0100

    Create sessions sign_in, sign_up and log_out actions + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  namespace :api, default: {format: json} do
+  namespace :api, default: {format: :json} do
     namespace :v1 do
       devise_scope :user do
         post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+        delete "log_out", to: "sessions#destroy"
       end
     end
   end
